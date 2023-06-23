c906b1ad3e7825fcbd032f0aa486098f7d5b13e2
ok66ym <yu.6.425@icloud.com>
2023-06-23 19:49:36 +0900

    create migration file and Norecord of posts table.

--- database/migrations/2023_06_23_182019_add_column_to_posts_table.php
+++ database/migrations/2023_06_23_182019_add_column_to_posts_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('image', 100) -> nullable();  //nullable：NULL値を許容．
         });
     }
 
@@ -26,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 };
